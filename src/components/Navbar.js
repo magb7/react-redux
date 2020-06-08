6282e205ffede7bc92fd34df47dd0d459486dbbd
magb7 <margaux.bedu@gmail.com>
2020-06-08 12:16:53 +0200

    create a components folder

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Navbar extends Component {
+  render() {
+    return (
+      <nav className="Navbar">
+        <h1>My awesome counter</h1>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
