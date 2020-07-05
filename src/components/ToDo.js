7755eb7eb9965a40447f451772c317790f29a2e6
magb7 <margaux.bedu@gmail.com>
2020-07-05 19:46:24 +0200

    add onClick function and modify style

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,20 +1,50 @@
 import React from "react";
 import styled from "styled-components";
 
-const ToDo = ({ completed, todo }) => {
-  return <StyledList completed={completed}>{todo}</StyledList>;
+const ToDo = ({ completed, todo, id }) => {
+  return (
+    <StyledList completed={completed}>
+      <li
+        onClick={() => {
+          console.log(`toggle task ${id}`);
+        }}
+      >
+        {todo}
+      </li>
+      <button
+        onClick={() => {
+          console.log(`delete task ${id}`);
+        }}
+      >
+        Delete
+      </button>
+    </StyledList>
+  );
 };
 
-const StyledList = styled.li`
+const StyledList = styled.div`
   display: flex;
   justify-content: space-around;
+  align-items: center;
   padding: 5px;
   width: 100%;
   border: 1px solid red;
   margin-bottom: 10px;
-  list-style: none;
-  text-decoration-line: ${(props) =>
-    props.completed ? "line-through" : "none"};
+
+  li {
+    list-style: none;
+    text-decoration-line: ${(props) =>
+      props.completed ? "line-through" : "none"};
+  }
+
+  button {
+    border-color: coral;
+    background-color: lightcoral;
+    padding: 5px;
+    border-radius: 5px;
+    outline: none;
+    font-weight: bold;
+  }
 `;
 
 export default ToDo;
