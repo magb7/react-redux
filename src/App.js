01c6f1e4c8ed1b2e03b70244baef5fc49d7064dc
magb7 <margaux.bedu@gmail.com>
2020-07-09 16:53:54 +0200

    add sign in component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import "./App.css";
 import SignUp from "./components/SignUp";
+import SignIn from "./components/SignIn";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <SignUp />
+        <SignIn />
       </div>
     );
   }
