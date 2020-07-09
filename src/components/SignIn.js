d5558831ec2733e16b4368c717abb16fc5b8c5df
magb7 <margaux.bedu@gmail.com>
2020-07-09 16:53:07 +0200

    implement sign in component

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -3,18 +3,18 @@ import styled from "styled-components";
 import axios from "axios";
 
 const SignUp = () => {
-  const [newUser, setNewUser] = useState({});
+  const [newConnection, setNewConnection] = useState({});
 
   const handleChange = (e) => {
-    const tmp = { ...newUser, [e.target.name]: e.target.value };
-    setNewUser(tmp);
+    const tmp = { ...newConnection, [e.target.name]: e.target.value };
+    setNewConnection(tmp);
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(newUser);
+    console.log(newConnection);
     axios
-      .post(`http://localhost:5000/auth/signup`, newUser)
+      .post(`http://localhost:5000/auth/signin`, newConnection)
       .then()
       .catch((err) => {
         console.log("Error:", err);
@@ -23,40 +23,13 @@ const SignUp = () => {
 
   return (
     <StyledWrapper>
-      <h3>{newUser.email}</h3>
-      <h3>{newUser.password}</h3>
-      <h3>{newUser.name}</h3>
-      <h3>{newUser.lastname}</h3>
+      <h2>Sign In Form</h2>
       <br />
       <form
         onSubmit={(e) => {
           handleSubmit(e);
         }}
       >
-        <label>
-          Name :
-          <input
-            type="text"
-            name="name"
-            required
-            onChange={(e) => {
-              handleChange(e);
-            }}
-          />
-        </label>
-        <br />
-        <label>
-          Lastname :
-          <input
-            type="text"
-            name="lastname"
-            required
-            onChange={(e) => {
-              handleChange(e);
-            }}
-          />
-        </label>
-        <br />
         <label>
           Email :
           <input
@@ -79,11 +52,6 @@ const SignUp = () => {
               handleChange(e);
             }}
           />
-        </label>
-        <br />
-        <label>
-          Password Bis :
-          <input type="password" name="passwordbis" required />
         </label>
         <br />
         <input type="submit" value="Submit" />
