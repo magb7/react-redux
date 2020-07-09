3aa1bfec7846d8f87b5747157592bcfef4ec77f1
magb7 <margaux.bedu@gmail.com>
2020-07-09 17:43:48 +0200

    implement react-router and routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
 import "./App.css";
 import SignUp from "./components/SignUp";
 import SignIn from "./components/SignIn";
+import Profile from "./components/Profile";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <SignUp />
-        <SignIn />
+        <h1>Login test</h1>
+        <Switch>
+          <Route exact path="/" component={SignIn} />
+          <Route path="/signin" component={SignIn} />
+          <Route path="/signup" component={SignUp} />
+          <Route path="/profile" component={Profile} />
+        </Switch>
       </div>
     );
   }
